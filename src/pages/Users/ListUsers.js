ac7756601a523fede4e8fb63b5238985c3922375
grachos <graziany@gmail.com>
2022-10-31 12:03:49 -0500

    Commit: Se creo el metodo elimiar usuaio

--- src/pages/Users/ListUsers.js
+++ src/pages/Users/ListUsers.js
@@ -8,17 +8,9 @@ import { Link, Navigate } from "react-router-dom";
 import APIInvoke from "../../utils/APIInvoke";
 import Navbar from "../../components/Header";
 import Footer from "../../components/Footer";
-//import swal from "sweetalert";
+import swal from "sweetalert";
 
 const ListUsers = () => {
-   /* const alerta = (msg, tipo, titulo) => {
-        swal({
-            title: titulo,
-            text: msg,
-            icon: tipo,
-            button: "Acept!"
-        })
-    }*/
     const [users, setUsers] = useState([]);
 
     const showUsers = async () => {
@@ -30,6 +22,28 @@ const ListUsers = () => {
     useEffect(() => {
         showUsers()
     }, []);
+
+    const deleteUser =  async (e, id) =>{
+        e.preventDefault(); 
+        await swal({
+            title: "¿Está seguro de eliminar el registro?",
+            text: "Una vez eliminado, no podrá recuperar el registro!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((regDelete) => {
+            if (regDelete) {
+                swal("El registro ha sido eliminado con exito", {icon: "success"});
+                const response = APIInvoke.invokeDELETE("/tickets/deluser/"+id);
+                console.log(response.msg); 
+                showUsers();
+            } else {
+                swal("No se eliminó el registro!");
+            }
+          });
+        
+    }
 
     const idUsuario = localStorage.getItem("id_user");
     if (idUsuario === null) {
@@ -69,9 +83,8 @@ const ListUsers = () => {
                                                             to={"#"}
                                                         >Actualizar</Link>
                                                         <button
-                                                            type="button"
                                                             className="btn btn-outline-danger"
-                                                            onClick={"#"}
+                                                            onClick={(e)=> deleteUser(e, item._id)}
                                                         > Eliminar</button>
                                                     </div>
                                                 </td>
