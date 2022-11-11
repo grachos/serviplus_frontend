22b50675cdd294fcfbcacc4f2bd3e4ed7f4a94cc
grachos <graziany@gmail.com>
2022-11-11 11:58:54 -0500

    Changing postion image on home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,10 +17,7 @@ const Home = () => {
                     <div className="container">
                         <h1 className="mt-5">Página principal ServiPlus.</h1>
                         <p className="lead">Sistema integrado de tickets que le permitirá registrar y hacer seguimiento a sus tickets. </p>
-                    </div>
-                    <div className="d-flex col-sm-8 px-1 justify-content-md-center">
                         <img src="../Tickets-icon.png" alt="Login" align="center" />
-
                     </div>
                 </main>
                 <Footer />
