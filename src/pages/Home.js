2585bb8cff2811d0c377a5843808ded9408b3a60
grachos <graziany@gmail.com>
2022-11-11 17:04:17 -0500

    Add images as icons to buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
                     <div className="container">
                         <h1 className="mt-5">Página principal ServiPlus.</h1>
                         <p className="lead">Sistema integrado de tickets que le permitirá registrar y hacer seguimiento a sus tickets. </p>
-                        <img src="../Tickets-icon.png" alt="Login" align="center" style={{ width:"70%", height: "70%" }}/>
+                        <img className="img-fluid" src="../Tickets-icon.png" alt="Login" align="center" style={{ width:"70%", height: "70%" }}/>
                     </div>
                 </main>
                 <Footer />
