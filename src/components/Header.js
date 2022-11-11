42dd2cefc0d2a4c448b45fbc19f982123e389046
grachos <graziany@gmail.com>
2022-11-11 15:05:22 -0500

    change button cerar sesion

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ const Navbar = ({ page }) => {
                         <li className="nav-item">
                             <Link className={`nav-link ${page === "ListTickets" ? "active" : ""}`} to={"/Listt"}>Tickets</Link>
                         </li>
-                        <li className="ms-xl-4 px-5"><button type="button" className="btn btn-primary" onClick={onClick}>Cerrar sesión</button></li>
+                        <li className="nav-item"><Link type="button" class="btn btn-outline-primary border border-light" onClick={onClick}>Cerrar sesión</Link></li>
                     </ul>
                     
                 </div>
